e336f5e0263df66bec5df536f4548c9f0af282a1
Hadayetullah <hadayetullah30@gmail.com>
2025-08-15 16:15:25 +0600

    Setup django-environ

--- portfolio_v2_api/settings.py
+++ portfolio_v2_api/settings.py
@@ -10,20 +10,29 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import environ
 from pathlib import Path
+
+# Initialise environment variables
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(BASE_DIR / '.env.local')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_7y+_5*j#w5bt8j^=gciodo4w)bgu(vfb+(1%pe+nc57ao@)py'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG', default=False)
 
 ALLOWED_HOSTS = []
 
@@ -31,7 +40,6 @@ AUTH_USER_MODEL = 'portfolio_v2.CustomUser'
 
 # CORS settings
 CORS_ORIGIN_WHITELIST = (
-    'http://localhost:8000',
     'http://localhost:3000',
 )
 
