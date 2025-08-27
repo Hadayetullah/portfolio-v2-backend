782f6a1f336f039f9dd58d4b41be0cd8df5b0287
Hadayetullah <hadayetullah30@gmail.com>
2025-08-27 20:53:22 +0600

    Create get method for the ManualSignupView.

--- portfolio_v2/admin.py
+++ portfolio_v2/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import CustomUser, UserAuthProvider, UserMessageContents
+from .models import CustomUser, UserAuthProvider, UserMessageContents, OTPCode
 
 # Register your models here.
 admin.site.register(CustomUser)
 admin.site.register(UserAuthProvider)
 admin.site.register(UserMessageContents)
+admin.site.register(OTPCode)
